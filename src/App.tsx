60791e308a181741aa0dac30fd4d5c5f3bcad542
SS007 <rbkmoneyukk@bk.ru>
2020-05-26 18:50:48 +0400

    ts03

--- src/App.tsx
+++ src/App.tsx
@@ -2,30 +2,32 @@ import React, {useState} from 'react';
 import './App.css';
 import {Todolist} from "./Todolist";
 import {createHook} from "async_hooks";
+import {v1} from "uuid";
 
 // Ошибки:
 // failed to compile: is not defined - не определено
 
-export type FilterValuesType = "All" | "Active" | "Completed" ;
+export type FilterValuesType = "All" | "Active" | "Completed" ; // тип пропсов для кнопок
 
 function App() {
 
+    //стейт тасок
     let [tasks, setTasks] = useState ([   // массив тасок const - неизменяемо
-        { id: 1, title: "HTML&CSS", isDone: true},
-        { id: 2, title: "JS", isDone: true},
-        { id: 3, title: "React js", isDone: false},
-        { id: 4, title: "TypeScript", isDone: true},
-        { id: 5, title: "Rest API", isDone: false},
-        { id: 6, title: "GraphQL", isDone: false}
+        { id: v1(), title: "HTML&CSS", isDone: true},
+        { id: v1(), title: "JS", isDone: true},
+        { id: v1(), title: "React js", isDone: false},
+        { id: v1(), title: "TypeScript", isDone: true},
+        { id: v1(), title: "Rest API", isDone: false},
+        { id: v1(), title: "GraphQL", isDone: false}
     ]);
 
-
-    function removeTask (id: number) {  // удаление таски
+    // удаление таски
+    function removeTask (id: string) {
         let filteredTasks = tasks.filter( (t) => t.id !== id) // метод filter массива пробегает по всем объектам и заносит в новый объект только те элементы, которые удоволетворяют условиям. в данном случае по ID
         setTasks (filteredTasks);
     }
 
-
+    // юзабельность кнопок алл, актив, комлитед
     let [filter, setFilter] = useState <FilterValuesType> ("All");
     let tasksForTodolist = tasks;
         if (filter === "Active") {
@@ -38,15 +40,26 @@ function App() {
         setFilter (value);
     }
 
+    // добавление новой таски
+    function addTask (title: string) {
+        let task = {
+            id: v1(),
+            title: title,
+            isDone: false
+        };
+        let newTasks = [task, ...tasks];
+        setTasks (newTasks);
+    }
 
 
     return (
-        <div className="App">
+        <div className={'App'}>
             <Todolist
                 title={"What to learn"} // заголовки компоненты
                 tasks={tasksForTodolist}  // сами таски
                 removeTask={removeTask} //  удаление таски
                 changeFilter={changeFilter} // юзабельность кнопок all active completed
+                addTask={addTask} // добавление таски
             />
         </div>
     );
--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -1,44 +1,88 @@
-import React from "react";
+import React, {ChangeEvent, KeyboardEvent, useState} from 'react';
 import {FilterValuesType} from "./App";
 
+// условия типов пропсов для тасок
 type TaskType = { //type какого типа будут таски, использующиеся в PropsType  Array<TaskType>
-    id: number
+    id: string
     title: string
     isDone: boolean
 }
 
-type PropsType = { // тип пропсов
+// условия типов пропсов для функции тудулист
+type PropsType = {
     title: string // в title можно писать только строку
     tasks: Array<TaskType> // тип массив объектов // type TaskType
     // tasks: TaskType []  - можно так же писать и будет работать
-    removeTask: (taskId: number) => void // удаление тасок происходит только по id и типы number
+    removeTask: (taskId: string) => void // удаление тасок происходит только по id и типу string
     changeFilter: (value: FilterValuesType ) => void // в changeFilter можно указать только строку и точное название ("All" |(<- или) "Active" |(<- или) "Completed") или алл или актив или комплетед. ТС будет следить за правильностью написания данных
+    addTask: (title: string) => void // функция которая принимает title string
 }
 
+
 export function Todolist (props: PropsType) { // props: any - что угодно, тоесть не задали четко тип, который будет отслеживаться
+
+    //стейт новых тасок.
+    const [newTaskTitle, setNewTaskTitle] = useState('');
+
+    // добавление новой таски
+    const addTask = () => { // в callback функции отдаем значение наверх в стейт апп
+        props.addTask(newTaskTitle); // в пропсах к нам приходит функция добавления новой таски. в эту функцию мы кладем значения новой таски из стейта новых тасок и передаем это в колбеках
+        setNewTaskTitle(''); // зануляем значение в input == очистить значение в стейте
+    }
+
+    // читаем введенные значения в инпуте и отправляем его наверх в апп с помощью колбек функции
+    const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        // e: ChangeEvent<HTMLInputElement> - сюда приходят значения с поля ввода ( простыми словами введенные, пользователем, значения.) ChangeEvent происходит с инпутом (HTMLInputElement)
+        // берем текушие значения из инпута(e: ChangeEvent<HTMLInputElement>)  и преобразуем их в функцию (setNewTaskTitle) добавления нового значения в title. значение отлавливаем из inputa (e.currentTarget.value)
+        setNewTaskTitle(e. //event сокрщ. е. - любое событие, которое происходит в объектной модели документа
+            currentTarget. // обработчик события (ловим событие) e.currentTarget Определяет элемент, в котором в данный момент обрабатывается событие, при движении события внутри DOM. currentTarget: это элемент, на который вы фактически связали событие. Это никогда не изменится. target: какой бы элемент ни был на самом деле нажат. Он может меняться, поскольку это может быть внутри элемента, к которому было связано событие.
+            value); // мы можем получить данные из этого поля ввода. А после, например, вывести их в консоль
+    }
+
+    // добавляем таску с ENTER
+    const onKeyPressHandler = (e: KeyboardEvent<HTMLInputElement>) => { //берем событие с клавиатуры из инпута  (e: KeyboardEvent<HTMLInputElement>) KeyboardEvent происходит с инпутом (HTMLInputElement)
+        if (e.charCode === 13) { // charCode это значение кнопок клавы на машином языке, понять номер каждой клавиши можно на сайте https://keycode.info/. если нажатие клавиши на клаве ентер, то по чаркоду это 13, если это равно по типу 13 то добавь новую таску
+            addTask(); // добавь таску
+        }
+    }
+
+    const onAllClickHandler = () => {props.changeFilter ("All")} // кнопка all отдает значение наверх и в APP уже меняется стейт
+    const onActiveClickHandler = () => {props.changeFilter ("Active")} // кнопка Active отдает значение наверх и в APP уже меняется стейт
+    const onCompletedClickHandler = () => {props.changeFilter ("Completed")} // кнопка Completed отдает значение наверх и в APP уже меняется стейт
+
     return (
         <div>
             <h3>{props.title}</h3> {/*заголовки тасок*/}
             <div>
-                <input/>
-                <button>+</button>
+                <input //поле ввода
+                    onChange={onChangeHandler} // callback // onChange показывает и позволяет изменять значения поля ввода
+                    value={newTaskTitle} // callback // получаем данные из поля ввода
+                    onKeyPress={onKeyPressHandler} // callback // (onKeyPress - когда клавиша нажата. onKeyDown - когда клавиша нажата, но не отпущена. onKeyUp когда клавиша поднята. onKeyDown+onKeyUp = onKeyPress) при нажатии enter добавляем таску
+                />
+                <button
+                    onClick={addTask} //при нажатии на кнопку вызываем функцию addTask и отдаем значение обратно наверх, где сработает функция добавления таски (addTask для app)
+                >+</button>
             </div>
             <ul>
                 {
-                    props.tasks.map ( // метод map на основе всех элементов создает новый массив с видоизменными элементами (другими объектами)
-                        (t) => <li>
+                    props.tasks.map ((t) => { // метод map на основе всех элементов создает новый массив с видоизменными элементами (другими объектами)
+
+                        const onClickHandler = () => props.removeTask(t.id) //при нажатии кнопки удаляется таска. ВАЖНО функция removeTask вызывается и туда залетают параметры с id и улетает назад в колбеке
+
+                        return (<li>
                             <input
                                 type="checkbox"
-                                checked={t.isDone}/> {/*галки*/}
+                                checked={t.isDone}/> {/*состояние галки*/}
                             <span>{t.title}</span> {/*сами таски*/}
-                            <button onClick={ () => {props.removeTask(t.id)} }>x</button> {/*при нажатии кнопки удаляется таска*/}
+                            <button onClick={onClickHandler}>x</button>
                         </li>)
+                        })
                 }
             </ul>
             <div>
-                <button onClick={ () => {props.changeFilter ("All")}}>All</button>
-                <button onClick={ () => {props.changeFilter ("Active")}}>Active</button>
-                <button onClick={ () => {props.changeFilter ("Completed")}}>Completed</button>
+                <button onClick={onAllClickHandler}>All</button>
+                <button onClick={onActiveClickHandler}>Active</button>
+                <button onClick={onCompletedClickHandler}>Completed</button>
             </div>
         </div>
     )
