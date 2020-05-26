a0134fed0d6ea9b21a1b11e098a3c42336c644d1
SS007 <rbkmoneyukk@bk.ru>
2020-05-26 12:06:22 +0400

    Updates

---
+++ precache-manifest.36b9499bc904fe9b2a0fc43dc530aa5f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06de30f6c87e00ca4bb77649e3057e2c",
+    "url": "/tstodolist/index.html"
+  },
+  {
+    "revision": "de70b16709323c1b48ba",
+    "url": "/tstodolist/static/css/main.6f30ddfc.chunk.css"
+  },
+  {
+    "revision": "44c9594272034858e2f3",
+    "url": "/tstodolist/static/js/2.6a65364b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tstodolist/static/js/2.6a65364b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de70b16709323c1b48ba",
+    "url": "/tstodolist/static/js/main.155d6275.chunk.js"
+  },
+  {
+    "revision": "ed58e65e5cc4c7a7e600",
+    "url": "/tstodolist/static/js/runtime-main.b69e365d.js"
+  }
+]);
