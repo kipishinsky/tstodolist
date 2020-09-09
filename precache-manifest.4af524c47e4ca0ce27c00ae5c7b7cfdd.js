e6b506c3ded0dae1b37d30339c5d7350902c950c
rbkmoneyukk@bk.ru <rbkmoneyukk@bk.ru>
2020-09-09 21:19:58 +0400

    Updates

--- precache-manifest.4af524c47e4ca0ce27c00ae5c7b7cfdd.js
+++ precache-manifest.4af524c47e4ca0ce27c00ae5c7b7cfdd.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "35f21258f3bac0cf441071d3294fa2d3",
+    "revision": "7d086f4a013211e41a2d041bd0b7bebd",
     "url": "/tstodolist/index.html"
   },
   {
-    "revision": "4d38d47bac9137293801",
+    "revision": "461e4cdb82ebc295d635",
     "url": "/tstodolist/static/css/main.5deae8a5.chunk.css"
   },
   {
-    "revision": "28726c4bb74acb212f5a",
-    "url": "/tstodolist/static/js/2.5a496391.chunk.js"
+    "revision": "43984d1ec6d1d8e5e137",
+    "url": "/tstodolist/static/js/2.25ae5ffb.chunk.js"
   },
   {
     "revision": "0749163b59fbee32225059cb60c18af6",
-    "url": "/tstodolist/static/js/2.5a496391.chunk.js.LICENSE.txt"
+    "url": "/tstodolist/static/js/2.25ae5ffb.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "4d38d47bac9137293801",
-    "url": "/tstodolist/static/js/main.c4e26f0b.chunk.js"
+    "revision": "461e4cdb82ebc295d635",
+    "url": "/tstodolist/static/js/main.e527f4b7.chunk.js"
   },
   {
     "revision": "eaa07748159daf28d081",
